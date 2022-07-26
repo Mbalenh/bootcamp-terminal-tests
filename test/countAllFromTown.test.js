fc75be44c5423a60f5d0a23bea9a17d35411852e
Mbalenh <3408534@myuwc.ac.za>
2022-07-26 16:02:08 +0200

    added files

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -11,7 +11,7 @@ describe("The countAllFromTown function" , function(){
     it("It should return 3 registrations from CA if there are three registration numbers from cape town", function(){
         assert.equal(1, countAllFromTown('CA 345 123, CJ 345, CJ 123','CA'));
     });
-    it("It should return 0 registrations from CJ if there are three registration numbers from Cj", function(){
+    it("It should return 0 registrations from CJ if there are no registration numbers from Cj", function(){
         assert.equal(0, countAllFromTown('CK 345 123, CK 345, CK 123','CJ'));
     });
    });
