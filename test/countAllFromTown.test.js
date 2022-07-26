165ab4e5bb9e1d6340157f577f1d2d721501979c
Mbalenh <3408534@myuwc.ac.za>
2022-07-26 13:22:10 +0200

    added modified files

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -2,16 +2,16 @@ import assert from "assert"
 import countAllFromTown from "../countAllFromTown.js"
 
 describe("The countAllFromTown function" , function(){
-    it("should  return  total number of the reg,if registration number startsWith('CL')", function(){
-        assert.equal(3, countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL'));
+    it("It should return 2 registrations from CL if there are two registration numbers from fromstellie", function(){
+        assert.equal(2, countAllFromTown('CL 124,CY 567,CL 345, CJ 456,','CL'));
     });
-    it("should return the total fromStellie registration,if the reg startsWith('CL')", function(){
+    it("It should return 3 registrations from CL if there are three registration numbers fromstellie", function(){
         assert.equal(3, countAllFromTown('CL 345 123, CL 345, CL 123','CL'));
     });
-    it("should return the total regCode fromCapeTown ,if the regCode startsWith('CA')", function(){
+    it("It should return 3 registrations from CA if there are three registration numbers from cape town", function(){
         assert.equal(1, countAllFromTown('CA 345 123, CJ 345, CJ 123','CA'));
     });
-    it("should return the total regCode that startsWith('CJ')", function(){
+    it("It should return 0 registrations from CJ if there are three registration numbers from Cj", function(){
         assert.equal(0, countAllFromTown('CK 345 123, CK 345, CK 123','CJ'));
     });
    });
--- test/countAllPaarl.test.js
+++ test/countAllPaarl.test.js
@@ -2,21 +2,21 @@ import assert from "assert"
 import countAllPaarl from "../countAllPaarl.js"
 
 describe("The countAllPaarl function" , function(){
-it("should return the total of the regNum that startsWith('CJ')", function(){
-    assert.equal(2, countAllPaarl('CJ 345 123, CK 345, CJ 123'));
-});
-it("should return the total of the regNum that startsWith('CJ')", function(){
-    assert.equal(1, countAllPaarl('CK 345 123, CK 345, CJ 123'));
-});
-it("should return the total of the regNum that startsWith('CJ')", function(){
-    assert.equal(3, countAllPaarl('CJ 345 123, CJ 345, CJ 123'));
-});
-it("shouldreturn the total of the regNum that startsWith('CJ')", function(){
-    assert.equal(0, countAllPaarl('CK 345 123, CK 345, CK 123'));
-});
-it("should  return the total of the regNum that startsWith('CJ')", function(){
-    assert.equal(0, countAllPaarl("NUZ 10") );
-});
-
-
-});
+    it("It should return 2 registrations from CJ if there are three registration numbers from Paarl", function(){
+        assert.equal(2, countAllPaarl('CJ 345 123, CK 345, CJ 123'));
+    });
+    it("It should return 1 registrations from CJ ,if registration numbers from paarl", function(){
+        assert.equal(1, countAllPaarl('CK 345 123, CK 345, CJ 123'));
+    });
+    it("It should return 3 registrations from CJ if there are three registration numbers from Paarl", function(){
+        assert.equal(3, countAllPaarl('CJ 345 123, CJ 345, CJ 123'));
+    });
+    it("It should return 0 registrations from CJ if there are no registration numbers from Paarl", function(){
+        assert.equal(0, countAllPaarl('CK 345 123, CK 345, CK 123'));
+    });
+    it("It should return 0 registrations from CJ if there are no registration numbers from Paarl", function(){
+        assert.equal(0, countAllPaarl("NUZ 10") );
+    });
+    
+    
+    });
